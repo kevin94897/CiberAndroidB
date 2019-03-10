da4fab386c8777ecf19de0ba3ed2160bc9aa69d1
Rafael <pineda.re@gmail.com>
2019-03-09 20:17:37 -0500

    Uso de fragments

---
+++ ActivityLifeCycle/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "LCYCLE";
+    Button btnA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnA = findViewById(R.id.btnActivity);
+
+        Log.d(TAG, "onCreate()");
+
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
+    }
+}
---
+++ AgendaFragment/app/src/main/java/com/example/agendafragment/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.agendafragment;
+
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Fragment fragmentDetail = new DetailContactFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.frDetailContact, fragmentDetail).commit();
+        }
+    }
+}
